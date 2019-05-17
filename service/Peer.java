e8bb70a645523f356bfeaaf9d030ea456475c87f
EstevesAndre <estevesandre1@hotmail.com>
2019-05-17 23:39:53 +0100

    changed to my ip address

--- service/Peer.java
+++ service/Peer.java
@@ -6,13 +6,13 @@ public class Peer {
 
     public static void main(String args[]) throws Exception {
         if (args.length == 0) { // test
-            new ChordNode("230.0.0.0", "9876");
+            new ChordNode("127.0.0.1", "9876");
         } else if (args.length == 1) { // new chord test
-            new ChordNode("230.0.0.0", args[0], "230.0.0.0", "9876");
+            new ChordNode("127.0.0.1", args[0], "127.0.0.1", "9876");
         } else if (args.length == 1) { // chord first node
-            new ChordNode("230.0.0.0", args[0]);
+            new ChordNode("127.0.0.1", args[0]);
         } else if (args.length == 3) { // new chord and existing given
-            new ChordNode("230.0.0.0", args[0], args[1], args[2]);
+            new ChordNode("127.0.0.1", args[0], args[1], args[2]);
         } else {
             System.err.println("Wrong number of arguments.\nUsage: java service/Peer <address_ip>? <address_port>");
             System.exit(-1);
