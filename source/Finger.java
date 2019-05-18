80be5c5a563738d14e08a8436e87b733bec71c2f
EstevesAndre <estevesandre1@hotmail.com>
2019-05-18 19:20:40 +0100

    add get methods

--- source/Finger.java
+++ source/Finger.java
@@ -1,13 +1,28 @@
 package source;
 
+import handlers.IOManager;
+
 public class Finger {
 
+    private String ID;
     private String address;
     private int port;
 
-    public Finger(String address, int port) {
+    public Finger(String address, int port) throws Exception {
         this.address = address;
         this.port = port;
+        this.ID = IOManager.getAddressHashID(address + '_' + port);
     }
 
+    public String getID() {
+        return ID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
